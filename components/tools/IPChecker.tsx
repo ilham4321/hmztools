fa7931e7b54a1807fedfa17647990eaac990b7ab
ilham4321 <ilhamwulan27@gmail.com>
2026-08-04 10:21:03 +0700

    https

--- components/tools/IPChecker.tsx
+++ components/tools/IPChecker.tsx
@@ -155,7 +155,7 @@ export function IPChecker({ title, description, article, dict }: IPCheckerProps)
     setIpInfo(null);
 
     try {
-      const response = await fetch('http://ip-api.com/json/');
+      const response = await fetch('https://ip-api.com/json/');
       const data = await response.json();
 
       if (data.status === 'success') {
